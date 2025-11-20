60aa0bfaedeebe796c84973dfeff5e5a52c98efb
wakkmiskun-bit <wakkmiskun@gmail.com>
2025-11-20 21:58:27 +0700

    {ilham}

--- Chapter0607_QUIS/ilham_714250003/fron.js
+++ Chapter0607_QUIS/ilham_714250003/fron.js
@@ -1,4 +1,11 @@
-// JavaScript untuk halaman konser - Versi Sederhana untuk Presentasi
+const waNumber = "6285191163819";
+
+// Fungsi untuk mengirim pesan WhatsApp
+function sendToWhatsApp(paket, harga) {
+    const message = `Halo, saya tertarik dengan paket ${paket} (Rp ${harga}). Bisa jelaskan lebih lanjut?`;
+    const url = `https://wa.me/${waNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
+}
 
 // Fungsi untuk mengubah tab jadwal
 function changeTab(day) {
