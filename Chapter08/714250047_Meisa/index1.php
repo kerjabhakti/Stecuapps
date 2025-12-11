95f429f77aa4851d9300d1ed91f3cc330eca8d76
meisa <zahrameisa463@gmail.com>
2025-12-11 17:55:51 +0700

    meisa

---
+++ Chapter08/714250047_Meisa/index1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan PHP dasar</title>
+</head>
+<body>
+    <h1>Latihan PHP</h1>
+    <h2>Hello World!</h2>
+    <?php 
+    echo "Saya Meisa Zahrah Nururrasyidah" ?>
+</body>
+</html>
---
+++ Chapter08/714250047_Meisa/php.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML pemula</title>
+</head>
+<body>
+    <h1>Helo <?php echo "Meisa Zahrah Nururrasyidah";?></h1>
+</body>
+</html>
