f9fc3a441b9eb4f4b05e5d5a044960745318b358
silvianans <silviananovitasari88@gmail.com>
2025-12-11 11:28:33 +0700

    Add files via upload

---
+++ Chapter08/714250051_Silviana/fungsi.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fungsi dari PHP</title>
+</head>
+<body>
+    <?php
+    function namaFungsinya_ya_gaiss() {
+        //block kode program
+    }
+    ?>
+</body>
+</html>
---
+++ Chapter08/714250051_Silviana/string.php
@@ -0,0 +1,14 @@
+ <?php
+    $nama = 'Silviana Novita Sari'; // tanda ptik satu
+
+    $pesan = "Selamat datang {$nama}"; // tanda petik dua
+    $pesan2 = 'selamat datang {$nama}'; // tanda petik satu
+
+    # ketika di - echo
+    echo $pesan , '<br>'; #selamat datang Silviana Novita Sari
+    echo $pesan2 , '<br>'; #selamat datang {$nama}
+?>
+
+
+
+
