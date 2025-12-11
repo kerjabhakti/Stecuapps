280ee8722d431bbd0102099530442914206137b7
leogrlly <jintenanggraeni6@gmail.com>
2025-12-11 11:24:15 +0700

    Add files via upload

---
+++ Chapter08/714250044_jinten/string.php
@@ -0,0 +1,8 @@
+<?php
+    $nama = 'Jinten Anggraeni';
+    $pesan = "Selamat Datang {$nama}";
+    $pesan2 = 'Selamat Datang {$nama}';
+
+    echo $pesan . '<br>';
+    echo $pesan2 . '<br>';
+    ?>
