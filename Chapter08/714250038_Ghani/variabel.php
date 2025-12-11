2be1ebaffa01f4b7b962efe8828473f193865bca
gaghannn <alghanimuhammadf@gmail.com>
2025-12-11 11:28:25 +0700

    php

---
+++ Chapter08/714250038_Ghani/variabel.php
@@ -0,0 +1,15 @@
+<?php
+    $nama = "Al Ghani Muhammad Fadhillah";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Halo, selamat datang <?php echo "Ghani";?></h1>
+</body>
+</html>
