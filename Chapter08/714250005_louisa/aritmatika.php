d6d4ac2dc9ace06855ae834841bd9ec0dfc61460
nandicaa <louisasitanggang17@gmail.com>
2025-12-22 19:54:25 +0700

    ica

---
+++ Chapter08/714250005_louisa/aritmatika.php
@@ -0,0 +1,16 @@
+<?php
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $s1 = "Hallo";
+    print ("Penjumlahan dan pengurangan : <br> \n");
+    printf ("$a + $b = %d <br> \n", $a + $b);
+    printf ("$a - $b = %d <br> \n", $a - $b);
+    print ("<br> perkalian : <br> \n");
+    printf ("$c * $b = %d <br> \n", $c * $b);
+    printf ("$c * 0.5 = %d <br> \n", $c * 0.5);
+    print ("<br> pembagian : <br> \n");
+    printf ("$c + $b = %d <br> \n", $c / $b);
+    printf ("$c / 3.5 = %d <br> \n", $c / 3.5);
+    printf ("<br> Yang ini menampilkan variabel string = $s1");
+?>
---
+++ Chapter08/714250005_louisa/php.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP pemula</title>
+</head>
+<body>
+    <h1>Helo <?php echo "Louisa Sitanggang";?></h1>
+</body>
+</html>
