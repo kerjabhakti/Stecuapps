f80e1dc72469dd340ecbf765979215311bbb9744
Riko Rizky Baswara <rikorizky@MacBook-Air-Riko.local>
2025-12-24 14:29:14 +0700

    perbaikan

--- 714250001_nabila/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysqli_connect("localhost", "root", "", "barista_cafe");
-
-if (!$db) {
-    die("Gagal koneksi database: " . mysqli_connect_error());
-}
-?>
--- 714250001_nabila/hapus.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("config.php");
-
-$id = $_GET['id'];
-mysqli_query($db, "DELETE FROM pendaftaran WHERE id=$id");
-
-header("Location: list-maba.php");
